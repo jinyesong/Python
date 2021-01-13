b3c30b5507957986c9a067a3c8623a0c466307a3
jinyesong <67140032+jinyesong@users.noreply.github.com>
2021-01-14 03:49:38 +0900

    Add files via upload

---
+++ base/practice2/2.2_while.py
@@ -0,0 +1,6 @@
+input_num = int(input())
+
+i = 1
+while i<=input_num:
+    print(str(i)+" "+str(i*i))
+    i += 1
---
+++ base/practice2/2.3_while.py
@@ -0,0 +1,7 @@
+height = 100
+
+i=1
+while i<=10:
+    height = height*0.6
+    print(str(i)+" "+str(round(height, 4)))
+    i += 1
