06b24d74cdd07a61ac1709989ba1be8ebb1af2ba
jinyesong <67140032+jinyesong@users.noreply.github.com>
2021-01-17 06:04:13 +0900

    Add files via upload

--- base/practice5/5.1_module.py
+++ base/practice5/5.1_module.py
@@ -1,1 +1,9 @@
-module
+import tkinter
+tkinter.widget = tkinter.Label(None, text='Hi Python!')
+tkinter.widget.pack()
+
+from tkinter import *
+widget = Label(None, text='Hi Python!')
+widget.pack()
+
+#del 모듈 -> 삭제
